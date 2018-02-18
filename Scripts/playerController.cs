b126ce75f85371a5150e6465cc2b4f1843737f5c
ElsaXing <elsaxingmiao@gmail.com>
2018-02-18 16:28:12 +0900

    move on phone done

--- Scripts/playerController.cs
+++ Scripts/playerController.cs
@@ -14,6 +14,8 @@ public class playerController : MonoBehaviour {
 		rb = GetComponent<Rigidbody>();
 		move.Set (0, 0, 0);
 		rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+
+
 	}
 	
 	// Update is called once per frame
@@ -36,9 +38,26 @@ public class playerController : MonoBehaviour {
 		}
 
 		//on phone
-		//if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Moved) {
-		//	x = Input.GetTouch (0).deltaPosition.x * speed;
-		//}
+		Rect topLeft = new Rect(0, 0, Screen.width / 2, Screen.height / 2);
+		Rect bottomLeft = new Rect(0, Screen.height / 2, Screen.width / 2, Screen.height / 2);
+		Rect right = new Rect(Screen.width / 2, 0, Screen.width / 2, Screen.height);
+
+		if (Input.touchCount > 0) {
+			var touchPos = Input.GetTouch(0).position;
+			if (topLeft.Contains(touchPos) && Input.GetTouch (0).phase == TouchPhase.Began)
+			{
+				z = 1.2f;
+			}
+			if (bottomLeft.Contains(touchPos) && Input.GetTouch (0).phase == TouchPhase.Began)
+			{
+				z = -1.2f;
+			}
+			if (right.Contains(touchPos) && Input.GetTouch (0).phase == TouchPhase.Moved)
+			{
+				x = Input.GetTouch (0).deltaPosition.x * speed / 100;
+			} 
+
+		}
 
 		move.Set (-x, 0, z);
 		rb.velocity = move / Time.deltaTime;
